5277cd461c83f3f8132f7b099f9cc297653d8275
DhruvCoderZX <83870107+DhruvCoderZX@users.noreply.github.com>
2021-07-17 16:10:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image(get_image){
+    fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
+        image_object=Img;
+        
+        image_object.scaleToWidth(600);
+        image_object.scaleToHeight(510);
+        image_object.set({
+            top:0,
+            left:0
+        });
+        canvas.add(image_object);
+    });  
+}
+
+
+function playSound(){
+	x.play();
+}
